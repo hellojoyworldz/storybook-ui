0bb569c2bebbf93ba52f98fedc8692a6b9da3325
hellojoyworldz <hellojoyworldz@gmail.com>
2024-09-16 23:48:41 +0900

    feat: main.tsx에 index.css 파일 import

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import '@fontsource/noto-sans-kr'
 import '@fontsource/noto-sans-kr/400.css'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
