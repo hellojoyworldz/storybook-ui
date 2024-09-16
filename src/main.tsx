0ddaac3d65be5198855377b1ccd0563059b2a2b0
hellojoyworldz <hellojoyworldz@gmail.com>
2024-09-16 20:07:49 +0900

    chore: fontsource 설치 및 폰트 설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import '@fontsource/noto-sans-kr'
+import '@fontsource/noto-sans-kr/400.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      body: ['Noto Sans KR'],
+    },
     fontSize: {
       xs: [
-        "12px",
+        '12px',
         {
-          lineHeight: "18px",
-          letterSpacing: "0",
-          fontWeight: "400",
+          lineHeight: '18px',
+          letterSpacing: '0',
+          fontWeight: '400',
         },
       ],
       sm: [
-        "14px",
+        '14px',
         {
-          lineHeight: "21px",
-          letterSpacing: "0",
-          fontWeight: "400",
+          lineHeight: '21px',
+          letterSpacing: '0',
+          fontWeight: '400',
         },
       ],
-      xl: ["20px"],
-      "2xl": [
-        "24px",
+      xl: ['20px'],
+      '2xl': [
+        '24px',
         {
-          lineHeight: "36px",
-          letterSpacing: "0",
-          fontWeight: "700",
+          lineHeight: '36px',
+          letterSpacing: '0',
+          fontWeight: '700',
         },
       ],
     },
     colors: {
-      primary: " #1d2745",
-      secondary: "#1de5d4",
-      tertiary: "#f52c50",
-      white: " #ffffff",
-      mono100: "#f1f1f1",
-      mono200: "#bebebe",
-      mono300: "#d6d7d9",
-      error: " #d01e1e",
-      social: "#395997",
+      primary: ' #1d2745',
+      secondary: '#1de5d4',
+      tertiary: '#f52c50',
+      white: ' #ffffff',
+      mono100: '#f1f1f1',
+      mono200: '#bebebe',
+      mono300: '#d6d7d9',
+      error: ' #d01e1e',
+      social: '#395997',
     },
   },
   plugins: [],
-};
+}
